5f4b069e02afbade1d81e88e47cf7c878c9ad204
Tareq Mahmood <tareqtms@yahoo.com>
2017-02-21 13:20:38 +0600

    Show all columns of standard sheet.

--- App/Actions/App.php
+++ App/Actions/App.php
@@ -214,21 +214,13 @@ class App extends Presentation {
         // Get server data
         $serverData = $_SESSION['serverData'];
         $titles = reset($serverData);
-        $newTitles = array();
-
-        foreach($mapsData as $key => $title) {
-            $titleKey = array_keys($titles, $title)[0];
-            $newTitles[$titleKey] = $title;
-        }
-
-        array_splice($serverData, 0, 1, array($newTitles));
 
         $exportData = array();
 
         foreach ($clientMapData as $k => $row) {
             $rowData = array();
-            foreach ($newTitles as $title) {
-                $rowData[$title] = $row[$title];
+            foreach ($titles as $title) {
+                $rowData[$title] = isset($row[$title]) ? $row[$title] : '';
             }
             $exportData[] = $rowData;
         }
