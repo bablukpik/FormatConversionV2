aba84a06e56cff3eb36bc281333f5a3c2cc8319d
MD. Bablu Mia <jacosmia@gmail.com>
2017-03-09 20:31:00 +0600

    Config file changed

--- Config/Config.php
+++ Config/Config.php
@@ -2,8 +2,8 @@
 namespace Config;
 
 class Config { //testdb04.ccf8wvvwgvwp.ap-northeast-1.rds.amazonaws.com
-    public static $dbHost = 'localhost'; //10.128.10.10
+    public static $dbHost = '10.128.10.10'; //10.128.10.10
     public static $dbName = 'convert-tool'; //convert-tool
-    public static $dbUser = 'root'; //convert-tool
-    public static $dbPass = ''; //6lnDGPcCmQn4xE01
+    public static $dbUser = 'convert-tool'; //convert-tool
+    public static $dbPass = '6lnDGPcCmQn4xE01'; //6lnDGPcCmQn4xE01
 }
